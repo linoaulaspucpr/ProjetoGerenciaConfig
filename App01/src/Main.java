212acf537e56adae31bebf0e252f249caa45f9a2
linoaulaspucpr <linoaulaspucpr@gmail.com>
2022-05-06 14:21:19 -0300

    conclusao tarefa deletar

--- App01/src/Main.java
+++ App01/src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Mudança branch cadastro: atualizaçao");
-        System.out.println("Conclusão branch cadastro");
+        System.out.println("Mudança branch listar: concluido");
+        System.out.println("atualizacao branch deletar");
+        System.out.println("Conclusão branch deletar");
         
         
     }
