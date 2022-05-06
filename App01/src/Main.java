16de998c23282d3568b20969ddfd338ed84e4d76
linoaulaspucpr <linoaulaspucpr@gmail.com>
2022-05-06 14:14:33 -0300

    criacao opcao de listagem

--- App01/src/Main.java
+++ App01/src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Mudança branch cadastro: atualizaçao");
         System.out.println("Conclusão branch cadastro");
+        System.out.println("Criacao da opcao de listagem");
+        
         
         
     }
