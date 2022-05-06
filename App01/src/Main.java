3812be51b935439b4cc12ab9482fb440f54686cf
linoaulaspucpr <linoaulaspucpr@gmail.com>
2022-05-06 14:10:58 -0300

    atualizacao

--- App01/src/Main.java
+++ App01/src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Mudança branch cadastro");
+        System.out.println("Mudança branch cadastro: atualizaçao");
         System.out.println("Conclusão branch cadastro");
+        
         
     }
 }
