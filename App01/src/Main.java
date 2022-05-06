8b170d505de43a44deabed2ec6ed0941a096fe37
linoaulaspucpr <linoaulaspucpr@gmail.com>
2022-05-06 14:01:56 -0300

    add tarefa de cadastro

--- App01/src/Main.java
+++ App01/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Mudança branch cadastro");
+        System.out.println("Conclusão branch cadastro");
+        
     }
 }
